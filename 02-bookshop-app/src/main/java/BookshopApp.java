69e4ea8a32802b013dbfa29e55f93d27e898c9cc
John Hunt <john.hunt10@gmail.com>
2020-05-26 12:32:38 +0100

    Updates to align with intro course

--- 02-bookshop-app/src/main/java/BookshopApp.java
+++
@@ -1,8 +0,0 @@
-
-public class BookshopApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Java Bookshop");
-        System.out.println("============================");
-    }
-}
-
