b8bc796a4818efa042624e7106aa24f27b349598
John Hunt <john.hunt10@gmail.com>
2020-11-26 17:40:07 +0000

    Updated ages

--- 06-methods/src/main/java/com/jjh/people/PersonApp.java
+++ 06-methods/src/main/java/com/jjh/people/PersonApp.java
@@ -3,19 +3,19 @@ package com.jjh.people;
 public class PersonApp {
 
 	public static void main(String[] args) {
-		Person p = new Person("John", 54);
+		Person p = new Person("John", 56);
 		System.out.println(p);
 		
 		System.out.println(p.getName() + " is " + p.getAge());
-		p.setAge(55);
+		p.setAge(57);
 		System.out.println(p.getName() + " is " + p.getAge());
 		
 		System.out.println(p);
 		
-		Person p1 = new Person("John", 55);
-		Person p2 = new Person("Denise", 52);
-		Person p3 = new Person("Phoebe", 22);
-		Person p4 = new Person("Adam", 20);
+		Person p1 = new Person("John", 56);
+		Person p2 = new Person("Denise", 53);
+		Person p3 = new Person("Phoebe", 23);
+		Person p4 = new Person("Adam", 21);
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
