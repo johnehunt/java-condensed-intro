aa365880da789ed7a1345321713934381e171c0c
John Hunt <john.hunt10@gmail.com>
2020-11-26 12:09:29 +0000

    Layout changes

--- 13-optional/src/main/java/com.jjh.optional/OptionalPositiveExample.java
+++ 13-optional/src/main/java/com.jjh.optional/OptionalPositiveExample.java
@@ -6,20 +6,20 @@ import java.util.stream.Collectors;
 
 public class OptionalPositiveExample {
 
-	public static void main(String[] args) {
-		List<Integer> values = Arrays.asList(1,-4, -3, 4, -10, 11, 0);
-		 List<Integer> allNegative = Arrays.asList(-1, -4, -3, -4,-10, -11);
-		 System.out.println( largestPositive(values) );
-		 System.out.println( largestPositive(allNegative) );
-	}
-	
-	public static int largestPositive(List<Integer> l ) {
-		  return l.stream()
-		      .filter(i -> i >= 0 )
-		      .sorted((x,y) -> y - x)
-		      .collect(Collectors.toList())
-		      .get(0);
-		 }
+    public static void main(String[] args) {
+        List<Integer> values = Arrays.asList(1, -4, -3, 4, -10, 11, 0);
+        List<Integer> allNegative = Arrays.asList(-1, -4, -3, -4, -10, -11);
+        System.out.println(largestPositive(values));
+        System.out.println(largestPositive(allNegative));
+    }
+
+    public static int largestPositive(List<Integer> l) {
+        return l.stream()
+                .filter(i -> i >= 0)
+                .sorted((x, y) -> y - x)
+                .collect(Collectors.toList())
+                .get(0);
+    }
 
 
 }
