2ab5ef0806f13b58668e6edfe2b18fc5022a138f
John Hunt <john@matraining.com>
2020-11-27 15:35:36 +0000

    Added Java8 style interface examples

---
+++ 10-interfaces/src/main/java/com/jjh/domain/Java8StyleInterface.java
@@ -0,0 +1,33 @@
+package com.jjh.domain;
+
+public interface Java8StyleInterface {
+    default void logInfo(String msg) {
+        log("Info", msg);
+    }
+
+    default void logWarning(String msg) {
+        log("Warning", msg);
+    }
+
+    default void logError(String msg) {
+        log("Error", msg);
+    }
+
+    private void log(String level, String msg) {
+        System.out.println(level + ": " + msg);
+    }
+
+    default void checkStatus(String status) {
+        if (isNull(status)) {
+            System.out.println(("Unknown status"));
+        }
+    }
+
+    private static boolean isNull(String str) {
+        return str == null ? true : "".equals(str) ? true : false;
+    }
+
+    void doIt();
+
+    int MAX = 100;
+}
