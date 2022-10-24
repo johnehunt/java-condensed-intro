39fc6d594938a3df8390a5fd2d972864aaeff9d9
John Hunt <john@matraining.com>
2022-10-24 15:17:08 +0100

    Added some comments

--- 04-classes/src/main/java/com/jjh/control/TestFor.java
+++ 04-classes/src/main/java/com/jjh/control/TestFor.java
@@ -2,6 +2,9 @@ package com.jjh.control;
 
 public class TestFor {
 
+	/*
+	 * Counted loop example
+	 */
 	public static void main(String[] args) {
 		for (int i=0; i< 10; i++) {
 			System.out.println(i);
--- 04-classes/src/main/java/com/jjh/control/TestWhile.java
+++ 04-classes/src/main/java/com/jjh/control/TestWhile.java
@@ -2,6 +2,9 @@ package com.jjh.control;
 
 public class TestWhile {
 
+	/**
+	 * Conditional loop example
+	 */
 	public static void main(String[] args) {
 		int i = 5, j = 10;
 		while (i < j) {
